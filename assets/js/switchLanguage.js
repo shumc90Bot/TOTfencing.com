75a0b93d580ceb29db7df2e61e9ea211b24ddfa5
AndreaC <149232074+shumc90Bot@users.noreply.github.com>
2024-02-15 16:44:43 +0800

    Add files via upload

---
+++ assets/js/switchLanguage.js
@@ -0,0 +1,57 @@
+// Switching language of Nav Bar
+const switchLanguageOnNavBar = document.querySelector("#languageBtn");
+    switchLanguageOnNavBar.addEventListener("click", function(){
+    engHomeBtn.textContent = "Home"
+    engAboutBtn.textContent = "About TOT"
+    engServiceBtn.textContent = "TOT Service"
+    engTeamBtn.textContent = "TOT Team"
+    engContactBtn.textContent = "Contact TOT"
+})
+// End of Switching language of Nav Bar
+
+
+// Switching language of About Section
+const switchLanguageOnAboutSection = document.querySelector("#languageBtn");
+    switchLanguageOnAboutSection.addEventListener("click", function() {
+    engAboutH2.textContent = "About TOT";
+    engAboutH3.textContent = "The Tao Te Ching says, \" The space between heaven and earth Isn't like a bellows?\" The term \"bellows\" refers to a wind instrument. We adopted its sound, \"TOT,\" as our name and took its meaning of \"being flexible yet resilient, moving and expanding\" to symbolize \"cultivating an open mind and maintaining an active spirit.\" We aspire to remain humble and continuously improve ourselves.";
+    engAboutH3.style.fontSize = "16px";
+    engAboutp1.textContent = "By using sports as a means, we aim to help our students and ourselves become better individuals. The English name for 拓躍劍擊 is TOT Fencing, where \"TOT\" represents children. We hope to maintain the innocence and positivity of children as we grow together with our students.";
+    engAboutp1.style.fontSize = "12px";
+    engAboutp2.textContent = "Founder Jonathan Tong Chun Kit is a former representative of the Hong Kong Fencing Team. After retiring, he served as Vice President of the Hong Kong Fencing Association and was a board member of one of Hong Kong's largest educational organizations, Po Leung Kuk, from 2018 to 2022. He held positions as supervisor or director in multiple schools and promoted fencing in several Po Leung Kuk-affiliated schools. In 2021, he established TOT Physical Education Company Limited, collaborating with various sports organizations to provide fencing training courses, venue design, and organize fencing competitions for different schools and institutions. In 2024, TOT Sports opened its first physical training center under the name TOT Fencing at Room 1601B, 68 Yee Wo Street, Causeway Bay. The venue occupies an area of over 3,000 square feet and features five international standard metal fencing strips, providing a more convenient, high-quality, and safe training environment for fencing enthusiasts on Hong Kong Island.";
+    engAboutp2.style.fontSize = "12px";
+    engAboutp3.textContent = "The coaching team at TOT is led by Head Coach Wong Chi Fai. The team consists of experienced coaches, most of whom are registered coaches of the Hong Kong Fencing Association, with years of experience in teaching fencing to children and youth. They have gained the trust of various schools and organizations. Currently, our teaching services are used by schools such as Pun U Association Wah Yan Primary School and Bishop Hall Jubilee School. Our collaborative partners include Po Leung Kuk, the Hong Kong Jockey Club Community Sports Committee, the Hong Kong Elite Athletes Association, and the Yau Tsim Mong District Community Sports Committee, among others.";
+    engAboutp3.style.fontSize = "12px";
+});
+// End of Switching language of About Section
+
+// Switching Language of Jonathan Section
+const switchLanguageOnJonathanSection = document.querySelector('#languageBtn');
+    switchLanguageOnJonathanSection.addEventListener("click", function(){
+    engTeamJonathan.textContent = "Founder of TOT - Jonathan Tong Chun Kit";
+    engTeamp1.textContent = "Jonathan Tong Chun Kit, a former representative of the Hong Kong Fencing Team, is a Level 2 Epee coach and referee certified by the Hong Kong Fencing Association. He comes from a family background in real estate business. After retiring from competitive fencing, he served as the Vice President of the Hong Kong Fencing Association. From 2018 to 2022, he held the position of Chief Executive Officer of Po Leung Kuk, overseeing multiple Po Leung Kuk-affiliated schools as Supervisor and Director. During his tenure as Supervisor at Po Leung Kuk Yu Chun Keung Memorial College, he established the first fencing training center under Po Leung Kuk, personally involved in the design of the school's fencing training venue, and integrated fencing into the school's physical education curriculum. The school has since developed into a \"New Territories East Children\'s Fencing Training Base and Competition Center,\" making fencing more accessible. Under Jonathan's guidance, the \"Little Swordsmen\" training program was launched, a collaboration between the Fly Beyond Sports Fund, the Hong Kong Fencing Association, Po Leung Kuk, and the sports social enterprise \"Sports Connect.\" The program recruited 450 grassroots primary school students from nine Po Leung Kuk-affiliated schools, providing them with systematic fencing training with the aim of transforming their lives through sports.";
+    engTeamp2.textContent = "Jonathan has a deep passion for fencing. In addition to assisting Hong Kong in hosting the 2017 Asian Fencing Championships, which was a great success, he also served as the team leader for the Hong Kong fencing delegation at the 2017 National Games and the 2019 National Youth Games, earning the respect and admiration of coaches and athletes.";
+    engTeamp3.textContent = "Jonathan has also successfully combined his business and fencing backgrounds by organizing various types of fencing competitions. These include the Po Leung Charity Fencing Competition (2022), which combines charity and sports elements, and the Yau Tsim Mong Youth Fencing Competition held at the Xiqu Centre, integrating Chinese and Western cultures, opera, and fencing.";
+    engTeamp4.textContent = "Jonathan also holds various positions in government sports-related committees and sports organizations, tirelessly promoting sports development.";
+    engTeamp5.textContent = "In 2021, Jonathan founded TOT Sports Limited and officially opened the first physical fencing training center under the name \"TOT Physical Fencing\" in Causeway Bay in 2024.";
+    jonaH1.textContent = "Fencing Qualifications:";
+    jonaListA1.textContent = "Former Hong Kong Fencing Team member";
+    jonaListA2.textContent = "Registered Level 2 Epee Coach, Hong Kong Fencing Association";
+    jonaListA3.textContent = "Registered Epee Referee, Hong Kong Fencing Association";
+    jonaH2.textContent = "Positions in Sports-Related Public Offices:";
+    jonaListB1.textContent = "Committee Member, Hong Kong Elite Athletes Association";
+    jonaListB2.textContent = "Vice President, Hong Kong Fencing Association (2016-2022)";
+    jonaListB3.textContent = "Committee Member, Active Hong Kong Fund Strategy Committee";
+    jonaListB4.textContent = "Advisor, Watsons Hong Kong Student Athlete Awards";
+    jonaH3.textContent = "Positions in Government-Related Committees:";
+    jonaListC1.textContent = "Sir David Trench Fund for Recreation - Committee Member";
+    jonaListC2.textContent = "Hong Kong Paralympic Fund Grants Committee - Committee Member";
+    jonaListC3.textContent = "Wan Chai District Fight Crime Committee - Committee Member";
+    jonaListC4.textContent = "Yau Tsim Mong District Youth Development and Civic Education Committee - Vice Chairman";
+    jonaListC5.textContent = "Yau Tsim Mong District Community Sports Committee - Chairman";
+    jonaH4.textContent = "Positions in Education-Related Public Offices:";
+    jonaListD1.textContent = "School Management Committee Member, Saint Francis University";
+    jonaListD2.textContent = "School Management Committee Member, Caritas Bianchi College of Careers";
+    jonaListD3.textContent = "Chief Executive Officer, Po Leung Kuk (2018-2022)";
+})
+// End of Switching Language of Jonathan Section
